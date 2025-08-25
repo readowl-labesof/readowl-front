48c2c768b60830c1ddd20d72d1000ce0dfaf4897
Luiz Alberto <luizandalecio19@gmail.com>
2025-08-25 20:05:45 -0300

    RO-17 docs(readme): Atualiza o readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ export default {
     extend: {
       colors: {
         'readowl-purple': {
-          DEFAULT: '#6750A4', // Roxo
-          light: '#9E85E0',   // Roxo Claro
-          dark: '#2F0959',    // Roxo Escuro
+          DEFAULT: '#6750A4',   // Roxo
+          light: '#9E85E0',     // Roxo Claro
+          dark: '#2F0959',      // Roxo Escuro
           extradark: '#23004B', // Roxo Extra Escuro
           extralight: '#F0EAFF' // Roxo Extra Claro
         },
