155b608d06f8d567f925293322f72991aa11d008
Luiz Alberto <luizandalecio19@gmail.com>
2025-08-25 20:05:45 -0300

    RO-17 style(tailwind config): Atualiza as cores padrões do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ export default {
     extend: {
       colors: {
         'readowl-purple': {
-          DEFAULT: '#6750A4', // Roxo
-          light: '#9E85E0',   // Roxo Claro
-          dark: '#2F0959',    // Roxo Escuro
+          DEFAULT: '#6750A4',   // Roxo
+          light: '#9E85E0',     // Roxo Claro
+          dark: '#2F0959',      // Roxo Escuro
           extradark: '#23004B', // Roxo Extra Escuro
           extralight: '#F0EAFF' // Roxo Extra Claro
         },
-        'readowl-purple-medium': '#836DBE',
+        'readowl-purple-medium': '#836DBE', // Roxo Médio
       }
     },
   },
